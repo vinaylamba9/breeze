0681c5dd47092788e9981ddc134218809175d09b
shekhar8897 <shekharshashank97@gmail.com>
2022-07-20 21:22:25 +0530

    project setup
    a) Backend using nodeJS
    b) Frontend using ReactJs

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats);
+})
+
+// app.get("/api/chat/:id", (req, res) => {
+//     const singleChat = chats.find(ch => ch._id == req.params.id);
+//     res.send(singleChat);
+// })
+app.listen(PORT, console.log(`SERVER Connected at ${PORT}`))
