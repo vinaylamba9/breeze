85a28bbb83e6f5c57c2b2837bead0ae0e7e5dfab
shekhar8897 <shekharshashank97@gmail.com>
2022-07-20 22:37:49 +0530

    Initiated Again with Tailwind

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,8 @@ app.get("/api/chat", (req, res) => {
     res.send(chats);
 })
 
-// app.get("/api/chat/:id", (req, res) => {
-//     const singleChat = chats.find(ch => ch._id == req.params.id);
-//     res.send(singleChat);
-// })
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find(ch => ch._id == req.params.id);
+    res.send(singleChat);
+})
 app.listen(PORT, console.log(`SERVER Connected at ${PORT}`))
