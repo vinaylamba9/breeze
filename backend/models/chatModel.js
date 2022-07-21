cf2b79d4b7fe1754c54d1daa79fe4c4a365cb400
shekhar8897 <shekharshashank97@gmail.com>
2022-07-21 14:02:02 +0530

    Created:- a) Mongoose Model Setup in nodeJS b) Network and Application in constant drive c) Added BasicUtils and DBUtils in nodeJS

---
+++ backend/models/chatModel.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const SchemaTypes = mongoose.Schema.Types;
+require("mongoose-long")(mongoose)
+
+const chatModel = mongoose.Schema({
+    chatName: {
+        type: String,
+        trim: true
+    },
+    isGroupChat: {
+        type: Boolean,
+        default: false
+    },
+    users: [{
+        type: SchemaTypes.ObjectId,
+        ref: "User"
+    }],
+    recentMessage: {
+        type: SchemaTypes.ObjectId,
+        ref: "Message"
+    },
+    groupAdmin: {
+        type: SchemaTypes.ObjectId,
+        ref: "User"
+    }
+}, {
+    timestamps: true,
+})
+
+module.exports = mongoose.model("Chat", chatModel);
