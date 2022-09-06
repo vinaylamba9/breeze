4af77b5ed5a5546037ab0cf4ed7cc27430218d82
shekhar8897 <shekharshashank97@gmail.com>
2022-09-06 19:28:27 +0530

    custom file upload done

--- breeze/src/components/fileUpload/fileUpload.js
+++ breeze/src/components/fileUpload/fileUpload.js
@@ -1,4 +1,4 @@
-import { Fragment, useRef, useEffect } from "react"
+import { Fragment, useRef } from "react"
 
 
 const FileUpload = ({ type, name, onChangeHandler, value, accept }) => {
@@ -12,6 +12,7 @@ const FileUpload = ({ type, name, onChangeHandler, value, accept }) => {
             <div>
                 <input ref={fileRef}
                     type={type}
+                    id="file"
                     className="rounded-3xl 
                         text-background-color-dark
                         w-60%  py-2.5
@@ -21,8 +22,9 @@ const FileUpload = ({ type, name, onChangeHandler, value, accept }) => {
                     name={name} accept={accept && accept}
                     onChange={onChangeHandler}
                 />
+                <label className="text-text-color-purity cursor-pointer flex w-60% rounded-3xl justify-center py-2.5 mb-2% bg-color-darkTeal" htmlFor="file">Choose your avatar</label>
             </div>
-        </Fragment>
+        </Fragment >
     )
 }
 
