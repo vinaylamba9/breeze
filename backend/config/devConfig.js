3557deb79f66bf3ae4a88abb7d55609a118fcc09
Shekhar S <shekharshashank97@gmail.com>
2023-08-18 16:19:02 +0530

    env configuration

--- backend/config/devConfig.js
+++
@@ -1,9 +0,0 @@
-const DevConfig = {
-	endPoints: "https://breezee.vercel.app/",
-	pingTimeout: 60000,
-	corsOrigin: {
-		origin: "https://breezee.vercel.app/",
-	},
-};
-
-module.exports = DevConfig;
